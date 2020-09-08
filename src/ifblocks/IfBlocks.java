d09a2ffb3fba7bc475d965d2ee69d0979aff9bb5
aprilhoward <apehow111@gmail.com>
2020-09-08 16:10:09 -0400

    saving score conditional statements

--- src/ifblocks/IfBlocks.java
+++ src/ifblocks/IfBlocks.java
@@ -5,35 +5,18 @@ public class IfBlocks {
     public static void main(String[] args) {
 
         boolean gameOver = true;
-        int score = 800;
+        int score = 200;
         int levelCompleted = 8;
         int bonus = 100;
-/*
-        if(score < 5000 && score > 1000) {
-            System.out.println("Your score was less than 5000 but greater than 1000");
-        } else if(score < 1000) {
-            System.out.println("your score was less than 1000");
+
+        if(score >= 5000) {
+            System.out.println("Your score is >= to 5000");
+        } else if(score < 1000 && score >= 500) {
+            System.out.println("Your score is < 100 but >= 500");
+        } else {
+            System.out.println("Your score is < 500");
         }
 
-        else{
-            System.out.println("Got here");
-        }
-
- */
-        if(gameOver) {
-            int finalScore = score + (levelCompleted * bonus);
-            System.out.println("Your final score was " + finalScore);
-        }
-
-        boolean newGameOver = true;
-        int newScore = 1000;
-        int newLevelCompleted = 10;
-        int newBonus = 200;
-
-        if(newGameOver) {
-            int finalScore = newScore + (newLevelCompleted * newBonus);
-            System.out.println("Your score is now " + finalScore);
-        }
 
 
     }
